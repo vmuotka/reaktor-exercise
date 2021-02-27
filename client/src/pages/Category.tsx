2becd085aaa37bb27967e4a919cc63babee426da
Jakk3 <32640696+Jakk3@users.noreply.github.com>
2021-02-27 11:01:06 +0200

    filter items by name

--- client/src/pages/Category.tsx
+++ client/src/pages/Category.tsx
@@ -12,14 +12,16 @@ interface ParamTypes {
 const Category: FC = () => {
   const { category } = useParams<ParamTypes>()
   const [items, setItems] = useState([])
-  const [loadedItems, setLoadedItems] = useState(50) // limit the initial rendered items to the first 50
+  const [filteredItems, setFilteredItems] = useState([])
+  const [renderLimit, setRenderLimit] = useState(50) // limit the initial rendered items to the first 50
   const [loading, setLoading] = useState(true)
+  const [filter, setFilter] = useState('')
 
   // API-request to backend
   useEffect(() => {
     setItems([])
     setLoading(true)
-    setLoadedItems(50)
+    setRenderLimit(50)
     axios.get(`/api/category/${category}`)
       .then(response => {
         setItems(response.data)
@@ -34,19 +36,34 @@ const Category: FC = () => {
     // detect when user has scrolled to the bottom of the screen and load more items
     const handleScroll = () => {
       if ((window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) && !loading) {
-        setLoadedItems(loadedItems + 50)
+        if (filteredItems.length > renderLimit) // only render more items if the current cap is filled
+          setRenderLimit(renderLimit + 50)
       }
     }
 
     window.addEventListener('scroll', handleScroll)
     return () => window.removeEventListener('scroll', handleScroll)
-  }, [loadedItems, loading])
+  }, [renderLimit, loading])
+
+  useEffect(() => {
+    setFilteredItems(items.filter((item: ItemEntry) => item.name.toLowerCase().includes(filter.toLowerCase())))
+  }, [items, filter])
 
   return (
     <div>
+      <form onSubmit={(e) => e.preventDefault()} className='flex justify-center my-2'>
+        <input
+          className='border border-primary-400 hover:border-primary-600 focus:border-primary-600 px-2 py-1 rounded focus:outline-none'
+          type='text'
+          placeholder='filter by name...'
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+        />
+      </form>
       {loading && <div className='w-full text-center text-gray-600 text-lg my-2'>Loading data...</div>}
+
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3 m-2'>
-        {items.slice(0, loadedItems).map((item: ItemEntry) =>
+        {filteredItems.slice(0, renderLimit).map((item: ItemEntry) =>
           <div key={item.id} className='bg-primary-300 p-2'>
             <ShirtIcon className='w-1/2 mx-auto my-1' />
             <ul>
